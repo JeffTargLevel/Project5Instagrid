37ba5f3eafe0e3c151d98755976bf477288de721
JeffGitHub <jeff.santolaria05@gmail.com>
2018-06-28 18:23:42 +0200

    Create file Instagrid

---
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by Jean-François Santolaria on 26/06/2018.
+//  Copyright © 2018 OpenClassroomsFRSantolariaJF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
